c84139ce668bce3d17d3a36a80f3586f14137026
Pub Quiz Iasi <pubquiziasi@gmail.com>
2024-06-14 10:47:34 +0300

    update domain url

--- be/utils/getCorsOrigin.ts
+++ be/utils/getCorsOrigin.ts
@@ -1,4 +1,4 @@
 import { isLocalDevelopment } from './isLocalDevelopment';
 
 export const getCorsOrigin = () =>
-  isLocalDevelopment ? 'http://localhost:3000' : 'https://pub-quiz-iasi.vercel.app';
+  isLocalDevelopment ? 'http://localhost:3000' : 'https://www.pubquiziasi.ro';
