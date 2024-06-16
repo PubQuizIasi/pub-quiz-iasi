260e7813aaee1c34da9ff19c93cb2827a9763bf4
Pub Quiz Iasi <pubquiziasi@gmail.com>
2024-06-16 15:30:52 +0300

    trigger deploy

--- be/routes/userRoutes.ts
+++ be/routes/userRoutes.ts
@@ -4,7 +4,7 @@ import { login, refreshToken, register } from '../controllers/userController';
 const router = Router();
 
 router.post('/login', login);
-router.post('/refresh-token', refreshToken);
 router.post('/register', register);
+router.post('/refresh-token', refreshToken);
 
 export default router;
